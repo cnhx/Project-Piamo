0a6fc709b0e023b52ff2e76baac930193f79fc62
Cnhx <i@cnhx.im>
2014-08-05 21:18:55 +0800

    Updates
    Add Jump assets
    Add Jump script

--- Piamo/Assets/Scripts/Player.cs
+++ Piamo/Assets/Scripts/Player.cs
@@ -7,13 +7,16 @@ public class Player : MonoBehaviour {
 	public Vector2 maxVelocity = new Vector2(3,5);
 	public float jetSpeed = 15f;
 	public float airSpeedMultiplier = .3f;
+	public float currentSpeed = 0f;
 
 	private PlayerController controller;
 	private bool isStanding = true;
+	private Animator animator;
 	
 	void Start(){
 
 		controller = GetComponent<PlayerController> ();
+		animator = GetComponent<Animator> ();
 
 		}
 	// Update is called once per frame
@@ -31,17 +34,28 @@ public class Player : MonoBehaviour {
 		}
 
 		if (controller.moving.x != 0) {
-			if(absVelX < maxVelocity.x){
-				forceX = isStanding ? (speed * controller.moving.x) : (speed * controller.moving.x * airSpeedMultiplier);
-				transform.localScale = new Vector3(forceX > 0 ? .6f: -.6f, .6f, 1);
+						if (absVelX < maxVelocity.x) {
+								currentSpeed = forceX = isStanding ? (speed * controller.moving.x) : (speed * controller.moving.x * airSpeedMultiplier);
+								transform.localScale = new Vector3 (forceX > 0 ? .6f : -.6f, .6f, 1);
+								animator.SetInteger ("AnimState", 1);
+								rigidbody2D.AddForce(new Vector2(forceX,forceY));
+						}
+				} else {
+						animator.SetInteger ("AnimState", 0);
+						rigidbody2D.AddForce(new Vector2(forceX,forceY));
 		}
-		}
+				
 
 		if (controller.moving.y > 0) {
-				if (absVelY < maxVelocity.y) 
+				if (absVelY < maxVelocity.y){ 
 						forceY = jetSpeed * controller.moving.y;
-				}
+						animator.SetInteger ("AnimState", 2);
+			
+				}else if(absVelY>0){
+				animator.SetInteger ("AnimState", 3);
+			}
 
 		rigidbody2D.AddForce(new Vector2(forceX,forceY));
+		}	
 }
 }
