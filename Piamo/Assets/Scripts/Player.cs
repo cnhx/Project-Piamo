d2baa8f6bcca8be5217e5434faf62762b5863c06
Cnhx <i@cnhx.im>
2014-08-05 17:15:43 +0800

    Update the scripts
    Add movement script

---
+++ Piamo/Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(3,5);
+	
+	
+	// Update is called once per frame
+	void Update () {
+		var forceX = 0f;
+		var forceY = 0f;
+		
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		
+		if (Input.GetKey("right")){
+			if(absVelX < maxVelocity.x){
+				forceX = speed;
+
+				transform.localScale = new Vector3(0.6,0.6,1); //flip the character to match the direction
+			}
+		} else if(Input.GetKey("left")){
+			if(absVelX < maxVelocity.x){
+				forceX = -speed;
+				
+				transform.localScale = new Vector3(-0.6,0.6,1);
+			}
+		}
+		
+		rigidbody2D.AddForce(new Vector2(forceX,forceY));
+	}
+}
