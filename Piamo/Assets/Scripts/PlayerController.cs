1016d01693b9306d60ee76f2a6f84bf223b4d229
cnhx <i@cnhx.im>
2014-08-07 16:26:25 +0800

    Update to 6th
    Update to 6th

--- Piamo/Assets/Scripts/PlayerController.cs
+++ Piamo/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 	
 	public Vector2 moving = new Vector2();
-	
+
+	public 
+
 	
 	// Use this for initialization
 	void Start () {
@@ -15,19 +17,34 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		
 		moving.x = moving.y = 0;
+
 		
-		if(Input.GetKey("right")) 
-		{
-			moving.x = 1;
-		} else if(Input.GetKey("left")){
-			moving.x = -1;
+//		if(Input.GetKey("right")) 
+//		{
+//			moving.x = 1;
+//		} else if(Input.GetKey("left")){
+//			moving.x = -1;
+//		}
+//		
+//		if(Input.GetKey("up")){
+//			moving.y = 1;
+//		}
+//		else if (Input.GetKey("down")){
+//			moving.y = -1;
+//		}
+
+		foreach (Touch touch in Input.touches) {
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)
+			{
+				if(touch.position.x<(Screen.width/2.0f)){
+					moving.y = 1;
+				}else{
+					moving.x = 1;
+				}
+			}
+			
 		}
-		
-		if(Input.GetKey("up")){
-			moving.y = 1;
-		}
-		else if (Input.GetKey("down")){
-			moving.y = -1;
-		}
+
+	
 	}
 }
